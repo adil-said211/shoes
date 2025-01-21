50ff1fc4864c1d254d1ff923b659f31186410a03
adil-said211 <adilsaid217@gmail.com>
2025-01-21 18:34:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// bach mni n cliki 3la l icons l border dyal l button li kayna wast mno l button yt7ayd lih l border
+// document.getElementById("toggler").addEventListener("click", function() {
+//     document.getElementById("toggler").style.setProperty("color", "#285430", "important"); // fi Js makandiroch !important kandiro imporan bla 3alama ta3ajob
+// });
+
+
+// code source site web nike?
